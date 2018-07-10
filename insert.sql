35c9fba935c0ef26fd37e5cfd6f45182e8e0b70f
Missy Norman <missy.norman412@gmail.com>
2018-07-10 16:08:04 -0400

    added insert.sql

---
+++ insert.sql
@@ -0,0 +1,20 @@
+insert into bamazon.products (product_name, department_name, price, stock_quantity) values
+("product 1", "dept 1", 1, 25);
+insert into bamazon.products (product_name, department_name, price, stock_quantity) values
+("product 2", "dept 2", 12, 25);
+insert into bamazon.products (product_name, department_name, price, stock_quantity) values
+("product 3", "dept 3", 1, 25);
+insert into bamazon.products (product_name, department_name, price, stock_quantity) values
+("product 4", "dept 4", 1, 25);
+insert into bamazon.products (product_name, department_name, price, stock_quantity) values
+("product 5", "dept 5", 1, 25);
+insert into bamazon.products (product_name, department_name, price, stock_quantity) values
+("product 6", "dept 6", 1, 25);
+insert into bamazon.products (product_name, department_name, price, stock_quantity) values
+("product 7", "dept 1", 1, 25);
+insert into bamazon.products (product_name, department_name, price, stock_quantity) values
+("product 8", "dept 1", 1, 25);
+insert into bamazon.products (product_name, department_name, price, stock_quantity) values
+("product 9", "dept 9", 15, 2);
+insert into bamazon.products (product_name, department_name, price, stock_quantity) values
+("product 10", "dept 1", 1, 25);
